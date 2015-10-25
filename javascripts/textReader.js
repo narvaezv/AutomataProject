6fb63cf30bf49bb22b88eb3ecef44e7b8e477df4
narvaezv <narvaezv94@gmail.com>
2015-10-25 15:46:13 -0600

    textReader javascript

---
+++ javascripts/textReader.js
@@ -0,0 +1,19 @@
+// JavaScript source code
+window.onload = function () {
+    var fileInput = document.getElementById('file');
+    var fileDisplayArea = document.getElementById('fileDisplayArea');
+
+    fileInput.addEventListener('change', function (e) {
+        var file = fileInput.files[0];
+        var textType = /text.*/;
+        if (file.type.match(textType)) {
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                fileDisplayArea.innerText = reader.result;
+            }
+            reader.readAsText(file);
+        } else {
+            fileDisplayArea.innerText = "File not supported!"
+        }
+    });
+}
