fcfa20c5f628a5c4cff070361ee4ec5737d99ef8
narvaezv <narvaezv94@gmail.com>
2015-10-25 21:41:53 -0600

    qwe

--- javascripts/textReader.js
+++ javascripts/textReader.js
@@ -15,7 +15,7 @@ window.onload = function () {
                 var results = "<ul>";
                 for (var int = 0; int < lines.length; int++) { //Check each word
                     var words = lines[int].split(" ");//Split words
-                    alert("inside"+int);
+                    alert("inside"+lines.length);
                     for (var jint = 0; jint < words.length; jint++) {
                         var check = stringAutomata(words[int], search);
                     }
